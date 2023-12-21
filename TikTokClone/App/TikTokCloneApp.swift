52ef9815433a8867f1e9e5df3fbc2fcb4a1de393
Sergey Petrosyan <ipetriez@icloud.com>
2023-12-21 19:59:03 +0400

    Initial Commit

---
+++ TikTokClone/App/TikTokCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokCloneApp.swift
+//  TikTokClone
+//
+//  Created by Sergey Petrosyan on 21.12.23.
+//
+
+import SwiftUI
+
+@main
+struct TikTokCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
