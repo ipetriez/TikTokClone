02153b75956ef87efa08a3aadb6fb68f49ff516d
Sergey Petrosyan <ipetriez@icloud.com>
2023-12-25 16:35:11 +0400

    Import AVKit and use VideoPlayer instead of sample rectangle inside FeedCell.

---
+++ TikTokClone/Feed/Post.swift
@@ -0,0 +1,8 @@
+//
+//  Post.swift
+//  TikTokClone
+//
+//  Created by Sergey Petrosyan on 25.12.23.
+//
+
+import Foundation
---
+++ TikTokClone/Feed/ViewModel/FeedViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  FeedViewModel.swift
+//  TikTokClone
+//
+//  Created by Sergey Petrosyan on 25.12.23.
+//
+
+import Foundation
